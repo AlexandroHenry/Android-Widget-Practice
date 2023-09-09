64352ef65b6e28b99037c5a9c0f0ac5ade46e34b
하승철 <alaxhenry0121@gmail.com>
2023-09-10 00:39:51 +0900

    RecyclerView 작업 진행 To Do: RecyclerView 에 좌우 스크롤 넣기
    RecyclerView 테이블 header 넣기
    
    RecyclerView 전체선택 checkbox 만들기

--- app/src/main/java/com/example/widgetpractice/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/widgetpractice/adapter/RecyclerAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.widgetpractice.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.widgetpractice.data.RecyclerData
 import com.example.widgetpractice.databinding.ListitemRecyclerviewBinding
@@ -10,12 +13,17 @@ class RecyclerAdapter(
     private val items: List<RecyclerData>,
     private val listener: AdapterListener
 ): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
         return ViewHolder(ListitemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context)))
     }
+
+    /**
+     * 정상코드
+     * */
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
@@ -24,8 +32,17 @@ class RecyclerAdapter(
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(private val view: ListitemRecyclerviewBinding) : RecyclerView.ViewHolder(view.root) {
+    inner class ViewHolder(private val view: ListitemRecyclerviewBinding) : RecyclerView.ViewHolder(view.root) {
         fun bind(item: RecyclerData, index: Int, listener: AdapterListener){
+
+//            view.noText.text = "NO"
+//            view.nameText.text = "Name"
+//            view.tickerText.text = "Ticker"
+//            view.closeText.text = "Price"
+//            view.highText.text = "High"
+//            view.lowText.text = "Low"
+//            view.volumeText.text = "Volume"
+
             view.noText.text = "${index + 1}"
             view.nameText.text = item.name
             view.tickerText.text = item.ticker
@@ -33,8 +50,6 @@ class RecyclerAdapter(
             view.highText.text = "${item.high}"
             view.lowText.text = "${item.low}"
             view.volumeText.text = "${item.volume}"
-
-            println(view.nameText.text)
         }
     }
 
